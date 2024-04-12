a4a0c555a4770e1cc35abf65ff0d54069cb5dc8a
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-04-12 22:05:51 +0900

    パッケージを全てアンインストールして、再度emacsを起動した。

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -87,3 +87,15 @@
 
 ;; (setq frame-title-format "%f")
 ;; (setq ring-bell-function 'ignore)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(macrostep leaf-convert leaf-tree leaf-keywords leaf)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
