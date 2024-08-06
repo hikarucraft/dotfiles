af7a2d045a3bb7316b2b43e7c9f0b32007d6b79a
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-08-06 19:12:32 +0900

    feat(nvim): add barbar.nvim

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,9 +1,13 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
-
 keymap("i", "\"", "\"\"<Left>", opts)
 keymap("i", "(", "()<Left>", opts)
 keymap("i", "{", "{}<Left>", opts)
 
-keymap("n","<C-l>",":<C-u>setlocal relativenumber!<CR>",opts)
+--barbar 
+keymap("n","<C-l>","<Cmd>setlocal relativenumber!<CR>",opts)
+keymap("n", "<C-k>", "<Cmd>BufferPrevious<CR>", opts)
+keymap("n", "<C-j>", "<Cmd>BufferNext<CR>", opts)
+keymap('n', '<C-c>', '<Cmd>BufferClose<CR>', opts)
+keymap('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,16 +1,27 @@
 return {
-  'lambdalisue/vim-fern',
-  keys = {
-    { "<C-n>", ":Fern . -reveal=% -drawer -toggle -width=80<CR>", desc = "toggle fern" },
-  },
-  dependencies = {
-    { 'lambdalisue/nerdfont.vim', },
-    {
-      'lambdalisue/fern-renderer-nerdfont.vim',
-      config = function()
-        vim.g['fern#renderer'] = "nerdfont"
-        vim.g['fern#default_hidden'] = 1
-      end
-    },
-  },
+         {
+           'lambdalisue/vim-fern',
+           keys = {
+             { "<C-n>", ":Fern . -reveal=% -drawer -toggle -width=80<CR>", desc = "toggle fern" },
+           },
+           dependencies = {
+             { 'lambdalisue/nerdfont.vim', },
+             {
+               'lambdalisue/fern-renderer-nerdfont.vim',
+               config = function()
+                 vim.g['fern#renderer'] = "nerdfont"
+                 vim.g['fern#default_hidden'] = 1
+               end
+             },
+           },
+         },
+         {
+           'romgrk/barbar.nvim',
+           dependencies = {
+             'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+             'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+           },
+           init = function() vim.g.barbar_auto_setup = false end,
+           opts = {},
+         }
 }
